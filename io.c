9e2a429d2ab7712eff414a4fff7924985cf924f4
dylan.bautista <dylan.bautista@estudiantat.upc.edu>
2024-11-30 11:59:41 +0100

    Added print character with color.

--- io.c
+++ io.c
@@ -4,6 +4,8 @@
 
 #include <io.h>
 
+#include <colors.h>
+
 #include <types.h>
 
 /**************/
@@ -35,8 +37,8 @@ void printc(char c)
   else
   {
     Word ch = (Word) (c & 0x00FF) | 0x0200;
-	Word *screen = (Word *)0xb8000;
-	screen[(y * NUM_COLUMNS + x)] = ch;
+    Word *screen = (Word *)0xb8000;
+    screen[(y * NUM_COLUMNS + x)] = ch;
     if (++x >= NUM_COLUMNS)
     {
       x = 0;
@@ -44,6 +46,40 @@ void printc(char c)
     }
   }
 }
+
+void print_color(char c, color_t char_col, color_t back_col, int blinking) {
+     __asm__ __volatile__ ( "movb %0, %%al; outb $0xe9" ::"a"(c)); /* Magic BOCHS debug: writes 'c' to port 0xe9 */
+  if (c=='\n')
+  {
+    x = 0;
+    y=(y+1)%NUM_ROWS;
+  }
+  else
+  {
+    Word ch = (Word) (c & 0x00FF) | (((char_col << 8 | back_col << 12) & 0x7FFF) | (blinking <<  15));
+    Word *screen = (Word *)0xb8000;
+    screen[(y * NUM_COLUMNS + x)] = ch;
+    if (++x >= NUM_COLUMNS)
+    {
+      x = 0;
+      y=(y+1)%NUM_ROWS;
+    }
+  }
+}
+
+void printc_xy_color(Byte mx, Byte my, char c)
+{
+  Byte cx, cy;
+  cx=x;
+  cy=y;
+  x=mx;
+  y=my;
+  printc(c);
+  x=cx;
+  y=cy;
+}
+
+
 
 void printc_xy(Byte mx, Byte my, char c)
 {
@@ -61,5 +97,5 @@ void printk(char *string)
 {
   int i;
   for (i = 0; string[i]; i++)
-    printc(string[i]);
+    print_color(string[i], Light_Gray,Green, 0);
 }
